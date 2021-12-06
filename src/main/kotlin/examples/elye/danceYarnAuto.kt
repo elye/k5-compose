8dc23204a870bbba7b9ee021e92d0e10e861039e
elye <elye.project@gmail.com>
2021-12-06 21:05:28 +1100

    Separate out yarn line drawing as a different function

--- src/main/kotlin/examples/elye/danceYarnAuto.kt
+++ src/main/kotlin/examples/elye/danceYarnAuto.kt
@@ -1,8 +1,10 @@
 package examples.elye
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerMoveFilter
 import k5
@@ -15,6 +17,7 @@ import kotlin.math.abs
 fun danceYarnAuto() = k5 {
     val noiseParamssGenerator = NoiseParamsGenerator()
     val mouseVector = Vector2D()
+
     show(modifier = Modifier
         .pointerMoveFilter(onMove = {
             mouseVector.x = it.x
@@ -25,25 +28,39 @@ fun danceYarnAuto() = k5 {
             val (m2d, m3d) = noiseParamssGenerator
             var offset = 0.0
             for (i in 0 until max(0f, mouseVector.x - 2).toInt()) {
-                fun noiseX(variant: Double) = 2 * dimensFloat.width * noise3D(variant, m2d, m3d)
-                fun noiseY(variant: Double) = 2 * dimensFloat.height * noise3D(offset + variant, m2d, m3d)
-                fun color(variant: Double) =  0xFF - (0xFF * noise3D(variant, m2d, m3d)).toInt()
-                val x = FloatArray(4) { index -> noiseX(offset + 5 + index * 10).toFloat() }
-                val y = FloatArray(5) { index -> noiseY(offset + 5 + index * 10).toFloat() }
-                val path = Path()
-                path.moveTo(x[0], y[0])
-                path.cubicTo(x[1], y[1], x[2], y[2], x[2], y[2])
-                val red = color(offset + 35)
-                val green = color(offset + 25)
-                val blue = color(offset + 15)
-                drawPath(path,
-                    Color(red, green, blue),
-                    alpha = max(min(mouseVector.y/dimensFloat.height, 1f), 0f),
-                    style = Stroke(width = 0.3f))
+                drawYarnLine(dimensFloat, offset, m2d, m3d, this, mouseVector)
                 offset += 0.002
             }
         }
     }
+}
+
+private fun drawYarnLine(
+    size: Size,
+    offset: Double,
+    m2d: Double,
+    m3d: Double,
+    drawScope: DrawScope,
+    mouseVector: Vector2D
+) {
+    fun noiseX(variant: Double) = 2 * size.width * noise3D(variant, m2d, m3d)
+    fun noiseY(variant: Double) = 2 * size.height * noise3D(offset + variant, m2d, m3d)
+    fun color(variant: Double) = 0xFF - (0xFF * noise3D(variant, m2d, m3d)).toInt()
+    val x = FloatArray(4) { index -> noiseX(offset + 5 + index * 10).toFloat() }
+    val y = FloatArray(5) { index -> noiseY(offset + 5 + index * 10).toFloat() }
+    val path = Path()
+    path.moveTo(x[0], y[0])
+    path.cubicTo(x[1], y[1], x[2], y[2], x[2], y[2])
+    val red = color(offset + 35)
+    val green = color(offset + 25)
+    val blue = color(offset + 15)
+    drawScope.drawPath(
+        path,
+        Color(red, green, blue),
+        alpha = max(min(mouseVector.y / size.height, 1f), 0f),
+        style = Stroke(width = 0.3f)
+    )
+
 }
 
 class NoiseParamsGenerator(
